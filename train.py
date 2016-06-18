1495beb0603f88d5cb9d00dcff18c13c62da6256
Jo Hill <joanneljhill@gmail.com>
2016-06-17 23:17:21 -0700

    Update n_estimators

--- train.py
+++ train.py
@@ -2,11 +2,27 @@ import os
 import pickle
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as mp
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+
+def plot_scores(X_train, X_test, y_train, y_test):
+    estimators = [1, 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    scores = []
+
+    for value in estimators:
+        clf = RandomForestClassifier(n_estimators=value)
+        clf = clf.fit(X_train, y_train)
+        scores.append(clf.score(X_test, y_test))
+
+    #plot data
+    mp.scatter(estimators,scores,color='blue')
+    mp.ylabel('n_estimators')
+    mp.xlabel('scores')
+    mp.savefig('estimators.png')
 
 def get_featurevector(image):
     img = Image.open(image).convert('L')
@@ -39,7 +55,7 @@ X, y = get_training_data(TRAINING_DATA)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.3, random_state=42)
 
 #Train classifier
-clf = RandomForestClassifier(n_estimators=10000)
+clf = RandomForestClassifier(n_estimators=200)
 clf = clf.fit(X_train, y_train)
 s = pickle.dump(clf, open('trained_model.p','w'))
 
