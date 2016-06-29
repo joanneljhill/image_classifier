a92efd346ff2975227cf85a84509b8edbc4cfd72
Jo Hill <joanneljhill@gmail.com>
2016-06-28 19:22:45 -0700

    Test feature generation, model training and evaluation

---
+++ get_trained_model.py
@@ -0,0 +1,18 @@
+from model_evaluation import *
+from feature_generation import *
+from model_training import *
+
+#Get training data
+X, y = get_training_data('training_data/')
+
+#Split training data
+X_train, X_test, y_train, y_test = split_data(X, y, 0.3)
+
+#Determine n_estimators to use
+#evaluate_n_estimators(X_train, X_test, y_train, y_test)
+
+#Train classifier
+train_random_forest(X_train, y_train, 200, 'model_1.p')
+
+#Evaluate model
+evaluate('model_1.p', X_test, y_test)
