7662697da6ea30da1e75c10f6064bb0b4311fa49
Jo Hill <joanneljhill@gmail.com>
2016-07-04 01:06:45 +0200

    Moving get_training_data function to workbench

--- feature_generation/feature_generation.py
+++ feature_generation/feature_generation.py
@@ -2,29 +2,6 @@ import os
 from PIL import Image
 import numpy as np
 
-def get_training_data(training_data_directory):
-    """
-    Takes labelled folders of images, featurizes them and returns training data that can be used to train models
-
-    :param training_data_directory: directory where the training data is stored
-    :return: X and y
-    """
-
-    X = []
-    y = []
-
-    for directory in os.listdir(training_data_directory):
-        if os.path.isdir(training_data_directory + directory + '/'):
-            for file in os.listdir(training_data_directory + directory + '/'):
-                if not file.startswith('.'):
-
-                    featurevector = get_feature_vector(training_data_directory + directory + '/' + file)
-
-                    X.append(featurevector)
-                    y.append(directory)
-
-    return X, y
-
 def get_feature_vector(image):
     """
     Converts an image to grayscale then resizes the image to 60 x 30
--- workbench/get_trained_model.py
+++ workbench/get_trained_model.py
@@ -2,6 +2,29 @@ from model_evaluation import *
 from feature_generation import *
 from model_training import *
 
+def get_training_data(training_data_directory):
+    """
+    Takes labelled folders of images, featurizes them and returns training data that can be used to train models
+
+    :param training_data_directory: directory where the training data is stored
+    :return: X and y
+    """
+
+    X = []
+    y = []
+
+    for directory in os.listdir(training_data_directory):
+        if os.path.isdir(training_data_directory + directory + '/'):
+            for file in os.listdir(training_data_directory + directory + '/'):
+                if not file.startswith('.'):
+
+                    featurevector = get_feature_vector(training_data_directory + directory + '/' + file)
+
+                    X.append(featurevector)
+                    y.append(directory)
+
+    return X, y
+
 #Get training data
 X, y = get_training_data('training_data/')
 
