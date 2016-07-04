91767006bc6c940e5520ee47db42ff7ea9146280
Jo Hill <joanneljhill@gmail.com>
2016-07-04 23:11:13 +0200

    Add workbench file

---
+++ workbench.py
@@ -0,0 +1,19 @@
+import argparse
+import pickle
+import helpers
+
+#train model
+helpers.train.get_trained_model('/Users/joannehill/ml/image_classifier/training_data/', 'model1.p')
+
+#get image files to classify
+parser = argparse.ArgumentParser()
+parser.add_argument('images', nargs='*', help='Image files to classify')
+args = parser.parse_args()
+
+#load trained model
+clf = pickle.load(open('model1.p'))
+
+#classify test images
+for image in args.images:
+    helpers.predict.get_text_classification(image)
+    helpers.predict.get_image_classification(image, clf)
